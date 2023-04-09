c5b197c86018944fca387f8ee5d5a05dd578cf8c
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 07:00:57 +0100

    adding more examples

--- example.js
+++ example.js
@@ -1,4 +1,16 @@
-const { searchPackage } = require("./bower-package");
+const { searchPackage, listPackages, packageExists } = require("./bower-package");
 
-console.log(searchPackage);
-console.log(searchPackage("jquery").then((results) => console.log(results)));
+// Search and get the results
+searchPackage("jquery").then((results) =>
+    console.log("Search results: " + results)
+);
+
+// Get the list of packages
+listPackages().then((results) =>
+    console.log("List of packages: " + results)
+);
+
+// Check a package exists
+packageExists("jquery").then((results) =>
+    console.log("Package exists: " + results)
+);
