b496b166c06a810ca38eae93448d48f0285fb428
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 07:02:06 +0100

    clean code

--- bower-package.js
+++ bower-package.js
@@ -1,42 +1,39 @@
 const axios = require('axios');
 
-const SEARCH_ENDPOINT = 'https://bower.herokuapp.com/packages/search/';
 const PACKAGES_ENDPOINT = 'https://bower.herokuapp.com/packages/';
+const SEARCH_ENDPOINT = PACKAGES_ENDPOINT + 'search/';
 
 // Send a GET request to the specified endpoint
 function sendRequest(url) {
-  return axios.get(url)
-    .then(response => response.data)
-    .catch(error => Promise.reject(error));
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(error => Promise.reject(error));
 }
 
 // Search for a package
 function searchPackage(packageName) {
-  const url = `${SEARCH_ENDPOINT}${packageName}`;
-  return sendRequest(url);
+    const url = `${SEARCH_ENDPOINT}${packageName}`;
+    return sendRequest(url);
 }
 
 // Get a list of packages
 function listPackages() {
-  return sendRequest(PACKAGES_ENDPOINT);
+    return sendRequest(PACKAGES_ENDPOINT);
 }
 
 // Check if a package exists
 function packageExists(packageName) {
-  const url = `${PACKAGES_ENDPOINT}${packageName}`;
-  return sendRequest(url)
-    .then(() => true)
-    .catch(error => {
-      if (error.response && error.response.status === 404) {
-        return false;
-      } else {
-        return Promise.reject(error);
-      }
-    });
+    const url = `${PACKAGES_ENDPOINT}${packageName}`;
+    return sendRequest(url)
+        .then(() => true)
+        .catch(error => {
+            if (error.response && error.response.status === 404) return false;
+            else return Promise.reject(error);
+        });
 }
 
 module.exports = {
-  searchPackage,
-  listPackages,
-  packageExists
+    searchPackage,
+    listPackages,
+    packageExists
 };
