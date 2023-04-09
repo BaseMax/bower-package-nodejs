4f36b47982d4a17b5e16256c72b76044a4149625
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 07:13:58 +0100

    convert spaces to tab

--- bower-package.js
+++ bower-package.js
@@ -7,67 +7,67 @@ let cachedPackages = null;
 
 // Send a GET request to the specified endpoint
 function sendRequest(url) {
-    return axios.get(url)
-        .then(response => response.data)
-        .catch(error => Promise.reject(error));
+	return axios.get(url)
+		.then(response => response.data)
+		.catch(error => Promise.reject(error));
 }
 
 // Search for a package
 function searchPackage(packageName) {
-    const url = `${SEARCH_ENDPOINT}${packageName}`;
-    return sendRequest(url);
+	const url = `${SEARCH_ENDPOINT}${packageName}`;
+	return sendRequest(url);
 }
 
 // Get a list of packages
 function listPackages(chunkSize = Infinity) {
-    return new Promise((resolve, reject) => {
-        // If the packages are already cached, return them
-        if (cachedPackages !== null) {
-            if (chunkSize === Infinity) {
-                resolve(cachedPackages);
-            } else {
-                // Split the cached packages into chunks
-                const chunks = [];
-                for (let i = 0; i < cachedPackages.length; i += chunkSize) {
-                    chunks.push(cachedPackages.slice(i, i + chunkSize));
-                }
-                resolve(chunks);
-            }
-        } else {
-            // Otherwise, send a request to the Bower registry
-            sendRequest(PACKAGES_ENDPOINT)
-                .then(packages => {
-                    // Cache the packages and return them
-                    cachedPackages = packages;
-                    if (chunkSize === Infinity) {
-                        resolve(cachedPackages);
-                    } else {
-                        // Split the packages into chunks
-                        const chunks = [];
-                        for (let i = 0; i < packages.length; i += chunkSize) {
-                            chunks.push(packages.slice(i, i + chunkSize));
-                        }
-                        resolve(chunks);
-                    }
-                })
-                .catch(error => reject(error));
-        }
-    });
+	return new Promise((resolve, reject) => {
+		// If the packages are already cached, return them
+		if (cachedPackages !== null) {
+			if (chunkSize === Infinity) {
+				resolve(cachedPackages);
+			} else {
+				// Split the cached packages into chunks
+				const chunks = [];
+				for (let i = 0; i < cachedPackages.length; i += chunkSize) {
+					chunks.push(cachedPackages.slice(i, i + chunkSize));
+				}
+				resolve(chunks);
+			}
+		} else {
+			// Otherwise, send a request to the Bower registry
+			sendRequest(PACKAGES_ENDPOINT)
+				.then(packages => {
+					// Cache the packages and return them
+					cachedPackages = packages;
+					if (chunkSize === Infinity) {
+						resolve(cachedPackages);
+					} else {
+						// Split the packages into chunks
+						const chunks = [];
+						for (let i = 0; i < packages.length; i += chunkSize) {
+							chunks.push(packages.slice(i, i + chunkSize));
+						}
+						resolve(chunks);
+					}
+				})
+				.catch(error => reject(error));
+		}
+	});
 }
 
 // Check if a package exists
 function packageExists(packageName) {
-    const url = `${PACKAGES_ENDPOINT}${packageName}`;
-    return sendRequest(url)
-        .then(() => true)
-        .catch(error => {
-            if (error.response && error.response.status === 404) return false;
-            else return Promise.reject(error);
-        });
+	const url = `${PACKAGES_ENDPOINT}${packageName}`;
+	return sendRequest(url)
+		.then(() => true)
+		.catch(error => {
+			if (error.response && error.response.status === 404) return false;
+			else return Promise.reject(error);
+		});
 }
 
 module.exports = {
-    searchPackage,
-    listPackages,
-    packageExists
+	searchPackage,
+	listPackages,
+	packageExists
 };
--- example.js
+++ example.js
@@ -2,38 +2,38 @@ const { searchPackage, listPackages, packageExists } = require("./bower-package"
 
 // Search and get the results
 searchPackage("jquery").then((results) => {
-    console.log("Search results: " + results.length);
-    // Iterate over the results
-    results.forEach((result) => {
-        console.log(result);
-    });
+	console.log("Search results: " + results.length);
+	// Iterate over the results
+	results.forEach((result) => {
+		console.log(result);
+	});
 });
 
 // Get the list of all packages
 listPackages().then((packages) => {
-    console.log("List of packages: " + packages.length);
+	console.log("List of packages: " + packages.length);
 
-    // Iterate over the results
-    packages.forEach((package) => {
-        console.log(package);
-    });
+	// Iterate over the results
+	packages.forEach((package) => {
+		console.log(package);
+	});
 });
 
 // Get the list of packages
 listPackages(20).then((chunks) => {
-    console.log("List of chunks: " + chunks.length)
-    console.log("List of packages: " + chunks.reduce((sum, chunk) => sum + chunk.length, 0));
+	console.log("List of chunks: " + chunks.length)
+	console.log("List of packages: " + chunks.reduce((sum, chunk) => sum + chunk.length, 0));
 
-    // Iterate over the results
-    chunks.forEach((chunk) => {
-        console.log(chunk);
-        // chunk.forEach((package) => {
-        //     console.log(package);
-        // });
-    });
+	// Iterate over the results
+	chunks.forEach((chunk) => {
+		console.log(chunk);
+		// chunk.forEach((package) => {
+		//     console.log(package);
+		// });
+	});
 });
 
 // Check a package exists
 packageExists("jquery").then((exists) => {
-    console.log("Package exists: " + exists)
+	console.log("Package exists: " + exists)
 });
